2d49ea70150ca1d58616f86a61586d314cd94627
Maireen Espiritu <imaireen@gmail.com>
2021-05-20 03:14:16 -0700

    implemented jsdoc

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@ import {MyToolkit} from './mytoolkit.js';
 
 // Implement a MyToolkit Button
 var btn = new MyToolkit.Button;
-btn.move(100,100);
+btn.move(40,70);
 btn.onclick(function(e){
 	console.log(e);
 });
@@ -15,7 +15,7 @@ btn.onWidgetStateChange(function(e) {
 
 // Implement a MyToolkit Checkbox
 var checkbox = new MyToolkit.Checkbox;
-checkbox.move(300, 100);
+checkbox.move(200, 70);
 checkbox.onclick(function(e) {
     console.log(e);
 });
@@ -26,7 +26,7 @@ checkbox.onWidgetStateChange(function(e) {
 
 // Implement a MyToolkit Radio Button
 var radiobutton = new MyToolkit.RadioButton(2);
-radiobutton.move(720, 100);
+radiobutton.move(580, 70);
 radiobutton.onclick(function(e) {
     console.log(e)
 });
@@ -38,7 +38,7 @@ radiobutton.onWidgetStateChange(function(e) {
 
 // Implement a MyToolkit Textbox
 var textbox = new MyToolkit.TextBox;
-textbox.move(970,80);
+textbox.move(200,320);
 textbox.onTextUpdate(function(e) {
     console.log(e);
 });
@@ -49,13 +49,13 @@ console.log(textbox.getText());
 
 // Implement a MyToolkit Scrollbar
 var scrollbar = new MyToolkit.ScrollBar;
-scrollbar.move(200,250);
+scrollbar.move(45,200);
 scrollbar.onThumbMove();
 scrollbar.setHeight(400);
 
 // Implement a MyToolkit Progress Bar
 var progressbar = new MyToolkit.ProgressBar;
-progressbar.move(420,250);
+progressbar.move(200,200);
 progressbar.setWidth(400);
 progressbar.setIncrementValue(30);
 progressbar.getIncrementValue();
@@ -66,3 +66,13 @@ progressbar.onIncrement(function (e) {
 progressbar.onWidgetStateChange(function (e) {
     console.log(e.type);
 })
+
+// CUSTOM: Implement a MyToolkit Toggle Button
+var togglebutton = new MyToolkit.ToggleButton;
+togglebutton.move(200, 480);
+togglebutton.onAction(function(e) {
+    console.log(e);
+});
+togglebutton.onWidgetStateChange(function(e) {
+    console.log(e.type);
+});
